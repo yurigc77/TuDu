2995a470167b1e7a2f6a36133f6639d4f21d7d06
Yuri Garcia Campos <yurigc@yahoo.com.br>
2023-05-10 15:14:52 -0300

    tela inicial e tela de cadastro

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuDu.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Task}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
